6ce9762196f8537e14320675990e9d18a2b9402f
Chris Dukakis <chrisdukakis@gmail.com>
2021-06-10 19:24:03 +0300

    convert to array first

--- src/serializeBinary.ts
+++ src/serializeBinary.ts
@@ -35,10 +35,11 @@ const ArrayBufferSerializer: Serializer<ArrayBuffer, string> = {
   // from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
   // modified to use Int8Array so that we can hold odd number of bytes
   toObject: async (ab: ArrayBuffer) => {
+    const a = new Int8Array(ab)
     let i = 0;
     let str = '';
-    while (i < ab.length) {
-      str += String.fromCharCode.apply(null, ab.subarray(i, Math.min((i += 0x8000), ab.length)));
+    while (i < a.length) {
+      str += String.fromCharCode.apply(null, a.subarray(i, Math.min((i += 0x8000), a.length)));
     }
     return str;
   },
